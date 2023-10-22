df4b6f3bfc9f763417e439f6d96d9021af6aba88
user-5039623452 <83167019+user-5039623452@users.noreply.github.com>
2023-10-22 16:36:38 +0300

    adjusted smallspinner size and created interface for props

--- src/components/Spinner/SmallSpinner.tsx
+++ src/components/Spinner/SmallSpinner.tsx
@@ -1,10 +1,19 @@
 import React from "react";
-import { ImDownload } from "react-icons/im";
 import { IoMdBicycle } from "react-icons/io";
-const SmallSpinner = () => {
+
+interface ISmallSpinner {
+  color?: string;
+  size?: number;
+}
+
+const SmallSpinner = ({ size, color }: ISmallSpinner) => {
   return (
     <div className="small-spinner-cont">
-      <IoMdBicycle className="small-spinner" size={40} color="rgb(70 70 70)" />
+      <IoMdBicycle
+        className="small-spinner"
+        size={size ? size : 20}
+        color={color ? color : "rgb(75, 75, 75)"}
+      />
     </div>
   );
 };
